0852544ee6318a6cc1f781a013997847f5b6e7c3
pepian <gp.pende@gmail.com>
2021-10-08 15:15:45 +0200

    Implementazione tastiera column left
    mancano attributi colore per FlatButton deprecated

--- lib/main.dart
+++ lib/main.dart
@@ -53,11 +53,17 @@ class _CalculatorHomePageState extends State<CalculatorHomePage> {
                 style: TextStyle(color: Colors.white),
                 ),
                 onPressed: (){deleteAll();},
+                style: TextButton.styleFrom(
+                  primary: Colors.white
+                ),
               ),
               TextButton(
                 child: Text(
                     '<-',),
                 onPressed: (){deleteOne();},
+                style: TextButton.styleFrom(
+                  primary: Colors.black87
+                )
               ),
             ],
           ),
@@ -70,21 +76,117 @@ class _CalculatorHomePageState extends State<CalculatorHomePage> {
                   Row(
                       crossAxisAlignment: CrossAxisAlignment.stretch,
                       children: <Widget>[
+                        TextButton(
+                          child: Text(
+                            '7',),
+                          onPressed: (){add('7');},
+                          style: TextButton.styleFrom(
+                            primary: Colors.white,
+                          )
+                        ),
+                        TextButton(
+                          child: Text(
+                            '8',),
+                          style: TextButton.styleFrom(
+                            primary: Colors.white,
+                          ),
+                          onPressed: () {add('8');},
+                          ),
+                        TextButton(
+                          child: Text(
+                            '9',),
+                          style: TextButton.styleFrom(
+                            primary: Colors.white,
+                          ),
+                          onPressed: () {add('9');},
+                        ),
                       ],
                   ),
                   Row(
                     crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: <Widget>[
+                      TextButton(
+                          child: Text(
+                            '4',),
+                          style: TextButton.styleFrom(
+                            primary: Colors.white,
+                          ),
+                          onPressed: () {add('4');},
+                        ),
+                      TextButton(
+                          child: Text(
+                            '5',),
+                          style: TextButton.styleFrom(
+                            primary: Colors.white,
+                          ),
+                          onPressed: () {add('5');},
+                        ),
+                      TextButton(
+                          child: Text(
+                            '6',),
+                          style: TextButton.styleFrom(
+                            primary: Colors.white,
+                          ),
+                          onPressed: () {add('6');},
+                        ),
                     ],
                   ),
                   Row(
                     crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: <Widget>[
+                      TextButton(
+                          child: Text(
+                            '1',),
+                          style: TextButton.styleFrom(
+                            primary: Colors.white,
+                          ),
+                          onPressed: () {add('1');},
+                        ),
+                      TextButton(
+                          child: Text(
+                            '2',),
+                          style: TextButton.styleFrom(
+                            primary: Colors.white,
+                          ),
+                          onPressed: () {add('2');},
+                        ),
+                      TextButton(
+                          child: Text(
+                            '3',),
+                          style: TextButton.styleFrom(
+                            primary: Colors.white,
+                          ),
+                          onPressed: () {add('3');},
+                        ),
                     ],
                   ),
                   Row(
                     crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: <Widget>[
+                      TextButton(
+                          child: Text(
+                            '0',),
+                          style: TextButton.styleFrom(
+                            primary: Colors.white,
+                          ),
+                          onPressed: () {add('0');},
+                        ),
+                      TextButton(
+                          child: Text(
+                            '.',),
+                          style: TextButton.styleFrom(
+                            primary: Colors.white,
+                          ),
+                          onPressed: () {add('.');},
+                        ),
+                      TextButton(
+                          child: Text(
+                            '=',),
+                          style: TextButton.styleFrom(
+                            primary: Colors.white,
+                          ),
+                          onPressed: () {getResult();},
+                        ),
                     ],
                   ),
                 ]
@@ -92,7 +194,11 @@ class _CalculatorHomePageState extends State<CalculatorHomePage> {
               Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-
+                  TextButton(
+                      onPressed: (){add('÷');},
+                      child: Image.asset("icons/divide.png",
+                      width: 10.0,
+                      height: 10.0,))
                 ],
               )
             ],
@@ -107,5 +213,9 @@ class _CalculatorHomePageState extends State<CalculatorHomePage> {
   deleteAll() {}
 
   deleteOne() {}
+
+  void add(String s) {}
+
+  void getResult() {}
 
 }
